8faab006aea8e412cd7343a31075893ab545aa2d
Piyush Malviya <malviyapiyush744@gmail.com>
2024-03-16 14:08:41 +0530

    dashboard

--- server/services/render.js
+++ server/services/render.js
@@ -4,26 +4,30 @@ const axios = require('axios');
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
-        .then(function(response){
-            res.render('index', { users : response.data });
+        .then(function (response) {
+            res.render('RANDOM', { users: response.data });
         })
-        .catch(err =>{
+        .catch(err => {
             res.send(err);
         })
 
-    
+
 }
 
-exports.add_user = (req, res) =>{
+exports.add_user = (req, res) => {
     res.render('add_user');
 }
+exports.upload = (req, res) => {
+    res.render('upload');
+}
 
-exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
-        .then(function(userdata){
-            res.render("update_user", { user : userdata.data})
+exports.update_user = (req, res) => {
+    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+        .then(function (userdata) {
+            res.render("update_user", { user: userdata.data })
         })
-        .catch(err =>{
+        .catch(err => {
             res.send(err);
         })
 }
+
