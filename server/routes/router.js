0e92bc64ab7726671af9af1a23a8fc91193693f9
Soham <sohamkelaskar@gmail.com>
2024-03-16 15:32:27 +0530

    login functionality implemented

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,34 +1,55 @@
-const express = require('express');
-const route = express.Router()
+const express = require("express");
+const route = express.Router();
 
-const services = require('../services/render');
-const controller = require('../controller/controller');
+const services = require("../services/render");
+const controller = require("../controller/controller");
+const Userdb = require("../model/model");
 
 /**
  *  @description Root Route
  *  @method GET /
  */
-route.get('/', services.homeRoutes);
-route.get('/upload', services.upload);
+route.get("/", services.homeRoutes);
+route.get("/upload", services.upload);
 
 /**
  *  @description add users
  *  @method GET /add-user
  */
-route.get('/add-user', services.add_user)
+route.get("/add-user", services.add_user);
 
 /**
  *  @description for update user
  *  @method GET /update-user
  */
-route.get('/update-user', services.update_user)
+route.get("/update-user", services.update_user);
 
+route.get("/login", services.login);
+
+route.post("/login", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const email = req.body.email;
+
+    const useremail = await Userdb.findOne({ email: email });
+    const passworduser = await Userdb.findOne({ password: password });
+    console.log(useremail);
+
+    if (password === passworduser) {
+      res.status(201).redirect("ProfilePage");
+    } else {
+      res.render("Random");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
+});
 
 // API
-route.post('/api/users', controller.create);
-route.get('/api/users', controller.find);
-route.put('/api/users/:id', controller.update);
-route.delete('/api/users/:id', controller.delete);
+route.post("/api/users", controller.create);
+route.get("/api/users", controller.find);
+route.put("/api/users/:id", controller.update);
+route.delete("/api/users/:id", controller.delete);
 
-
-module.exports = route
+module.exports = route;
--- server/services/render.js
+++ server/services/render.js
@@ -5,7 +5,7 @@ exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
         .then(function (response) {
-            res.render('RANDOM', { users: response.data });
+            res.render('ProfilePage', { users: response.data });
         })
         .catch(err => {
             res.send(err);
@@ -20,6 +20,30 @@ exports.add_user = (req, res) => {
 exports.upload = (req, res) => {
     res.render('upload');
 }
+exports.login = (req, res) => {
+    res.render('login');
+}
+// exports.login = async (req, res) => {
+//     try {
+//         const password = req.body.password;
+//         const email = req.body.email;
+    
+//         const useremail = await Userdb.findOne({ email: email });
+//         const passworduser = await Userdb.findOne({ password: password });
+        
+//         console.log(useremail);
+//         if (password === passworduser) {
+//           res.status(201).redirect("ProfilePage");
+//         } else {
+//           res.render("Random");
+//         }
+//       } catch (error) {
+//         res.status(400).send(error);
+//         console.log(error);
+//       }
+// }
+
+
 
 exports.update_user = (req, res) => {
     axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
